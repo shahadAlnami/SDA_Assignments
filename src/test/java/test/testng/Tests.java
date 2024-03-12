51a577b30de691f26d65592c0e55e8d2bdbeab7b
Shahad Alnami <shahadnamialnami@gmail.com>
2024-03-12 12:54:34 +0300

    first commit

---
+++ src/test/java/test/testng/Tests.java
@@ -0,0 +1,61 @@
+package test.testng;
+
+import engine.ActionsBot;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.config.Configurator;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.time.Duration;
+
+
+public class Tests {
+    protected WebDriver driver;
+    protected Wait<WebDriver> wait;
+    public static Logger logger;
+    public ActionsBot bot;
+    protected static JSONObject testData;
+
+
+
+
+    @BeforeClass
+    public void beforeClass() throws IOException, ParseException {
+        Configurator.initialize(null, "src/main/resources/properties/log4j2.properties");
+        logger = LogManager.getLogger(Tests.class.getName());
+        testData =  (JSONObject) new JSONParser().parse( new FileReader("src/test/resources/testData/sample.json", StandardCharsets.UTF_8) );
+    }
+
+    @BeforeMethod
+    public void beforeMethod(){
+        logger.info("Opening Chrome Browser");
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("start-maximized");
+        driver = new ChromeDriver(chromeOptions);
+
+        logger.info("Configuring 5 second explicit wait");
+        wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        bot = new ActionsBot(driver, wait, logger);
+    }
+
+    @AfterMethod
+    public void afterMethod(){
+        logger.info("Quitting Browser");
+        driver.quit();
+    }
+
+
+}
