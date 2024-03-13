78ffff68799840e2a030f14e6d8016422c05e4a0
Shahad Alnami <shahadnamialnami@gmail.com>
2024-03-13 16:20:39 +0300

    Added the details for task 6 and 5

--- src/test/java/test/junit/Week5_Task5.java
+++ src/test/java/test/junit/Week5_Task5.java
@@ -1,4 +1,19 @@
 package test.junit;
 
 public class Week5_Task5 {
+    //Store the path of the file as string and open the file.
+    //Open the workbook.
+    //Open the first worksheet.
+    //Go to the first row.
+    //Create a cell on the 3rd column (2nd index) on the first row.
+    //Write “POPULATION” on that cell.
+    //Create a cell on the 2nd row 3rd cell(index2), and write data.
+    //Create a cell on the 3rd row 3rd cell(index2), and write data.
+    //Create a cell on the 4th row 3rd cell(index2), and write data.
+    //Write and save the workbook.
+    //Close the file.
+    //Close the workbook.
+
+//The 3rd column on excel file must be empty before running.
+// Otherwise, new data will be written on the old data
 }
--- src/test/java/test/junit/Week5_Task6.java
+++ src/test/java/test/junit/Week5_Task6.java
@@ -1,4 +1,7 @@
 package test.junit;
 
 public class Week5_Task6 {
+    // Go to amazon.com
+    // Take Full Page Screenshot.
+    // Take any spesific WebElement ScreenShot
 }
