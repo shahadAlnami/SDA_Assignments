c3fb49e7c56b74bb6282c78a00eac45d68aa0285
Shahad Alnami <shahadnamialnami@gmail.com>
2024-03-15 13:47:37 +0300

    Just solved task3 week5

--- src/test/java/test/junit/Week5_Task3.java
+++ src/test/java/test/junit/Week5_Task3.java
@@ -1,5 +1,14 @@
 package test.junit;
 
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.interactions.Actions;
+
+import java.awt.*;
+import java.awt.event.KeyEvent;
+
 public class Week5_Task3 extends Tests{
     /**
      * Go to Amazon
@@ -8,4 +17,30 @@ public class Week5_Task3 extends Tests{
      * Click on the Amazon logo at the bottom of the page (in the footer) using JavascriptExecutor
      * Use Actions to type "ClarusWay" in the search box and perform the search
 */
+    @Test
+    public void amazonSearchTest() throws InterruptedException, AWTException {
+        bot.navigate("https://www.amazon.com");
+
+        // Scroll to the bottom of the page using a robot
+//        Robot robot = new Robot();
+//        robot.keyPress(KeyEvent.VK_END);
+//        robot.keyRelease(KeyEvent.VK_END);
+
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("window.scrollTo(0, document.body.scrollHeight)");
+
+        // Click on the "Back to top" web element
+        By backToTopButton = By.className("navFooterBackToTopText");
+        bot.click(backToTopButton);
+
+        // Click on the Amazon logo at the bottom of the page (in the footer) using JavascriptExecutor
+        By amazonLogo = By.xpath("//a[contains(@href,'/?ref_=footer_logo')]");
+        js.executeScript("arguments[0].click();", driver.findElement(amazonLogo));
+
+        // Use Actions to type "ClarusWay" in the search box and perform the search
+        By searchBox = By.id("twotabsearchtextbox");
+        Actions actions = new Actions(driver);
+        actions.sendKeys(driver.findElement(searchBox), "ClarusWay").sendKeys(Keys.ENTER).build().perform();
+
+    }
 }
