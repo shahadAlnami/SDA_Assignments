68e1879ee1630678fe071b1baa9e43fb79741c3a
Shahad Alnami <shahadnamialnami@gmail.com>
2024-03-17 14:45:09 +0300

    Just solved task3 week6

--- src/test/java/test/testng/Week6_Task3.java
+++ src/test/java/test/testng/Week6_Task3.java
@@ -1,5 +1,13 @@
 package test.testng;
 
+import engine.CustomListener;
+import org.openqa.selenium.By;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+@Listeners(CustomListener.class)
+
 public class Week6_Task3 extends Tests{
     /**
      * go to "https://practicetestautomation.com/practice-test-login/"
@@ -7,7 +15,26 @@ public class Week6_Task3 extends Tests{
      * enter password - "incorrectPassword"
      * and login
      * SOFT ASSERT the error message shown
-     * SOFT ASSERT the error message is "Your password is invalid!" */
+     * SOFT ASSERT
+     * the error message is "Your password is invalid!" */
 
+    @Test(testName = "Basic Soft Assertions Test", description = "Given I am on the practice login page, When I input valid credentials, Then I am logged in successfully.")
+    public void softAssertionsTest() {
+        bot.navigate("https://practicetestautomation.com/practice-test-login/");
+        By usernameInput = By.id("username");
+        By passwordInput = By.id("password");
+        By submitButton = By.id("submit");
+
+        bot.type(usernameInput,"student");
+        bot.type(passwordInput,"incorrectPassword");
+        bot.click(submitButton);
+
+       By errorMessage= By.id("error");
+        SoftAssert softAssert = new SoftAssert();
+        softAssert.assertTrue(driver.findElement(errorMessage).isDisplayed());
+        softAssert.assertEquals(driver.findElement(errorMessage).getText(),"Your password is invalid!");
+        softAssert.assertAll();
+
+    }
 
 }
